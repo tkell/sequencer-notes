2931e96c8d21ea70a213af84058617df16a9d764
Thor Kell <thor@tide-pool.ca>
2019-08-18 14:05:01 -0400

    nice grid

--- app/js/seek.js
+++ app/js/seek.js
@@ -1,24 +1,23 @@
 function drawLine(startX, startY, offsetX, offsetY) {
     var path = new Path();
-    path.strokeColor = 'black';
+    path.strokeColor = '#a4c3b5';
     var start = new Point(startX, startY);
     path.moveTo(start);
     path.lineTo(start + [offsetX, offsetY]);
 }
 
-
 function drawGrid(startX, startY, offsetX, offsetY, xLength, yLength, numRows, numCols) {
-    for (var i = 0; i < numRows; i++) {
+    for (var i = 0; i <= numRows; i++) {
         var x = startX;
         var y = startY + (offsetY * i);
         drawLine(x, y, xLength, 0);
     }
 
-    for (var i = 0; i < numCols; i++) {
+    for (var i = 0; i <= numCols; i++) {
         var x = startX + (offsetX * i);
         var y = startY
         drawLine(x, y, 0, yLength);
     }
 }
-
-drawGrid(5, 5, 20, 20, 400, 400, 20, 20);
+// Unsure why xLength is 1025 and not 1028?
+drawGrid(0, 0, 32, 32, 1025, 512, 16, 32);
