cdc6464ac3ac20ae9fb998200e747aaa0b65426a
Thor Kell <thor@tide-pool.ca>
2019-08-18 20:32:03 -0400

    drawing sequences

--- app/js/seek.js
+++ app/js/seek.js
@@ -1,23 +1,73 @@
-function drawLine(startX, startY, offsetX, offsetY) {
+// grid constants
+var gridSize = 32;
+
+function drawLine(startX, startY, offsetX, offsetY, color) {
     var path = new Path();
-    path.strokeColor = '#a4c3b5';
+    path.strokeColor = color;
     var start = new Point(startX, startY);
     path.moveTo(start);
     path.lineTo(start + [offsetX, offsetY]);
 }
 
-function drawGrid(startX, startY, offsetX, offsetY, xLength, yLength, numRows, numCols) {
+function drawGrid(startX, startY, xLength, yLength, numRows, numCols, color) {
     for (var i = 0; i <= numRows; i++) {
         var x = startX;
-        var y = startY + (offsetY * i);
-        drawLine(x, y, xLength, 0);
+        var y = startY + (gridSize * i);
+        drawLine(x, y, xLength, 0, color);
     }
 
     for (var i = 0; i <= numCols; i++) {
-        var x = startX + (offsetX * i);
+        var x = startX + (gridSize * i);
         var y = startY
-        drawLine(x, y, 0, yLength);
+        drawLine(x, y, 0, yLength, color);
     }
 }
+
+function drawGridCircle(x, y, color) {
+    var xLoc = x * gridSize;
+    var yLoc = y * gridSize;
+    var size = gridSize / 3
+    var circle = new Path.Circle(new Point(xLoc, yLoc), size);
+    circle.fillColor = color;
+}
+
+function drawGridLine(x1, y1, x2, y2, color) {
+    var xLoc1 = x1 * gridSize;
+    var yLoc1 = y1 * gridSize;
+    var xLoc2 = x2 * gridSize;
+    var yLoc2 = y2 * gridSize;
+    drawLine(xLoc1, yLoc1, xLoc2 - xLoc1, yLoc2 - yLoc1, color)
+}
+
+function drawSequence(points, color) {
+    // careful of the +=2 here,
+    // and the draw back to the beginning!
+    for (var i = 0; i < points.length; i+=2) {
+        // Last line
+        if (i === points.length - 2) {
+            var x1 = points[i];
+            var y1 = points[i + 1];
+            var x2 = points[0];
+            var y2 = points[1];
+            drawGridLine(x1, y1, x2, y2, color);
+        } else {
+            var x1 = points[i];
+            var y1 = points[i + 1];
+            var x2 = points[i + 2];
+            var y2 = points[i + 3];
+            drawGridLine(x1, y1, x2, y2, color);
+        }
+    }
+
+    // careful of the +=2 here!
+    for (var j = 0; j < points.length; j+=2) {
+        var x = points[j];
+        var y = points[j + 1];
+        drawGridCircle(x, y, color);
+    }
+}
+
 // Unsure why xLength is 1025 and not 1028?
-drawGrid(0, 0, 32, 32, 1025, 512, 16, 32);
+drawGrid(0, 0, 1025, 512, 16, 32, "#a4c3b5");
+drawSequence([5, 4, 5, 5, 5, 6, 6, 4], "green");
+
